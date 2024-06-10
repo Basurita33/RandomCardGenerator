ac832131157eaada2c0a8e08bbb511d6528e5e9c
Bruno Beceiro <164252484+Basurita33@users.noreply.github.com>
2024-06-10 06:54:35 +0000

    Basurita33 First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♥", "♦", "♣", "♠"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function getRandomCard() {
+    const suit = suits[Math.floor(Math.random() * suits.length)];
+    const value = values[Math.floor(Math.random() * values.length)];
+    return { suit, value };
+  }
+
+  function generateCard() {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    const card = getRandomCard();
+
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";
+
+    const colorClass = card.suit === "♥" || card.suit === "♦" ? "red" : "black";
+
+    const topLeftSuit = document.createElement("div");
+    topLeftSuit.className = `suit top-left ${colorClass}`;
+    topLeftSuit.innerText = card.suit;
+
+    const valueElement = document.createElement("div");
+    valueElement.className = colorClass;
+    valueElement.innerText = card.value;
+
+    const bottomRightSuit = document.createElement("div");
+    bottomRightSuit.className = `suit bottom-right ${colorClass}`;
+    bottomRightSuit.innerText = card.suit;
+
+    cardElement.appendChild(topLeftSuit);
+    cardElement.appendChild(valueElement);
+    cardElement.appendChild(bottomRightSuit);
+
+    cardContainer.appendChild(cardElement);
+  }
+
+  generateCard();
 };
